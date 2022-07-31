3cea3ba3a6abe382bbcbdec5f4cce8f367edc197
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-07-31 21:18:59 +1200

    fuck

--- Pong2/src/Pong2.java
+++ Pong2/src/Pong2.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 
 public class Pong2 extends JFrame {
-
+	private final static String WINDOW_TITLE = "Pong2";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
