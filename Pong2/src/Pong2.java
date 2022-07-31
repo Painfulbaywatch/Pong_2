99a42edf535fab0c4906598302cf19c3fda9deac
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-07-31 21:23:06 +1200

    another test, commits haven't been working

--- Pong2/src/Pong2.java
+++ Pong2/src/Pong2.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 
 public class Pong2 extends JFrame {
 	private final static String WINDOW_TITLE = "Pong2";
+	private final static int WINDOW_WIDTH = 800;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
