b89243063b372df2c121112b0b5007e1070fe6db
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-08-01 22:09:55 +1200

    Pong2 class completed

--- Pong2/src/Pong2.java
+++ Pong2/src/Pong2.java
@@ -3,9 +3,23 @@ import javax.swing.JFrame;
 public class Pong2 extends JFrame {
 	private final static String WINDOW_TITLE = "Pong2";
 	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	
+	public Pong2(){
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		add(new PongPanel2());
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Pong2();
+			}
+		});
 	}
 
 }
