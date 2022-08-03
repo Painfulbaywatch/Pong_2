3af8a44b4464c45ceceaffbbd1ad603766092c15
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-08-03 22:15:54 +1200

    still starting really, just saving to work on AS2 instead

---
+++ Pong2/src/PongPanel2.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+
+public class PongPanel2 {
+	
+	private final static Color PANEL_COLOUR = Color.black;
+	private final static int TIMER_DELAY = 5;
+	private final static int BALL_MOVEMENT_SPEED = 4;
+	private final static int POINTS_TO_WIN = 3;
+	private final static int XPADDING = 100;
+	private final static int YPADDING = 100;
+	private final static int FONT_SIZE = 50;
+	private final static String SCORE_FONT_FAMILY = "Serif";
+	
+}
